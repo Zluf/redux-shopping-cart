23bcd3889416a3e8ef15c5884e2f16c38c3ab007
Slav Konovalov <slav.konovalov@gmail.com>
2023-07-30 22:35:35 +0100

    initial commit: setting up of Redux store & data slices + implementing toggle Cart

---
+++ src/components/Shop/Products.js
@@ -0,0 +1,37 @@
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "My First Book",
+    description: "The First Book I've wrote",
+  },
+  {
+    id: "p2",
+    price: 5,
+    title: "My Second Book",
+    description: "The Second Book",
+  },
+];
+
+const Products = (props) => {
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.key}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
